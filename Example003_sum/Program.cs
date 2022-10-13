0cff91db4f850ba3b95f558d9397580577e32ad9
Komparon <one77@mail.ru>
2022-10-13 19:22:43 +0300

    Сумма случайных чисел с использованием Random

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -3,7 +3,14 @@
 // Console.WriteLine(numberA+ numberB);
 
 
-int numberA = 3;
-int numberB = 10;
+// int numberA = 3;
+// int numberB = 10;
+// int resus = numberA+numberB;
+// Console.WriteLine(resus);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int resus = numberA+numberB;
 Console.WriteLine(resus);
